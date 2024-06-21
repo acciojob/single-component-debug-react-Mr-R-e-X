187339fbcecacf990560495adc5bd0b425f2e0b5
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-21 12:49:12 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/App.js";
 
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
